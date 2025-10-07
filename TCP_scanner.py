eb1cc42188bde070362d045bcd89f5d7c5e4ea68
Rüveyde Orman <149947158+ruveydeorman@users.noreply.github.com>
2025-10-07 14:35:19 +0300

    Update TCP_scanner.py

--- TCP_scanner.py
+++ TCP_scanner.py
@@ -1,13 +1,15 @@
 import scapy.all as scapy
 import time
 def scanning_ports(TargetIP,PortRange):
-	ip_layer = scapy.IP(dst=TargetIP) # "dst" define where sending ip packet
-        tcp_layer = scapy.TCP(flags="S",dport=PortRange) # creating tcp packet "dport=target ports" 
-	combined_packet = ip_layer / tcp_layer  # this send SYN flag for specific port in target ip with tcp port
-	answered, unanswered = scapy.srp(combined_packet,timeout=2, verbose=False) # srp(Send and Receive Packets at Layer 2)
-	for sent, receive in answered:
-		if received.haslayer(scapy.TCP) and (received.getlayer(scapy.TCP).flags & 2): # checking SYN-ACK flags
-			print(f"[+]  {sent.dport} is open")
+	for port in PortRange:
+		ip_layer = scapy.IP(dst=TargetIP) # "dst" define where sending ip packet
+		tcp_layer = scapy.TCP(flags="S",dport=port) # creating tcp packet "dport=target ports" 
+		combined_packet = ip_layer / tcp_layer  # this send SYN flag for specific port in target ip with tcp port
+		answered, unanswered = scapy.sr(combined_packet,timeout=2, verbose=False) # srp(Send and Receive Packets at Layer 2)
+		for sent, receive in answered:
+			if receive.haslayer(scapy.TCP) and (receive.getlayer(scapy.TCP).flags & 2): # checking SYN-ACK flags
+				print(f"[+]  {sent.dport} is open")
+			
 if __name__=="__main__":
 	target = input("Target ip: ")
 	start = int(input("Start Port: "))
